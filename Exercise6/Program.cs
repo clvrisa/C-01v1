98b2e485aac6bde8f0165e04bb1e65b7af35bffd
clvrisa <77904575+clvrisa@users.noreply.github.com>
2021-07-05 20:54:23 -0700

    Exercise6

---
+++ Exercise6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6
+{
+    class Diamond
+    {
+        int rows, cols;
+
+        public void getRows()
+        {
+            Console.WriteLine("Enter number of rows: ");
+            rows = Convert.ToInt32(Console.ReadLine());
+        }
+        public void displayDiamond()
+        {
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int x = 1; x <= cols; x++)
+                {
+                    Console.WriteLine(" ");
+                    cols--;
+                }
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
