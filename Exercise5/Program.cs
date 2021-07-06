ba1f477a7afcfed9f421db5b95a68d80121e704d
clvrisa <77904575+clvrisa@users.noreply.github.com>
2021-07-05 20:53:59 -0700

    Exercise5

---
+++ Exercise5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise5
+{
+    class Binary
+    {
+        int rows, display;
+
+        public void getRows()
+        {
+            Console.WriteLine("Enter the Number of Rows: ");
+            rows = Convert.ToInt32(Console.ReadLine());
+        }
+        public void displayRows()
+        {
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int x = 1; x <= i; x++)
+                {
+                    if (display ==1)
+                    {
+                        Console.Write("0");
+                        display = 0;
+                    }
+                    else if (display == 0)
+                    {
+                        Console.Write("1");
+                        display = 1;
+                    }
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Binary binary = new Binary();
+            binary.getRows();
+            binary.displayRows();
+        }
+    }
+}
